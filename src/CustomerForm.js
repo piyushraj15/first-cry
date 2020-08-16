3e8cd3520fc52654e4d4014cc0e5e31593ff1578
piyushraj15 <piyush.raj73@gmail.com>
2020-08-16 17:29:36 +0530

    Add more attribute to the form

--- src/CustomerForm.js
+++ src/CustomerForm.js
@@ -8,9 +8,13 @@ import {MdEmail} from 'react-icons/md'
 import {TiContacts} from 'react-icons/ti'
 import {FaLandmark} from 'react-icons/fa'
 import {RiLockPasswordLine} from 'react-icons/ri'
-
-const postUrl="http://localhost:2000/add"
-
+import {AiFillWarning} from 'react-icons/ai'
+import countryList from 'react-select-country-list'
+const country= countryList().data
+console.log(country)
+const postUrl="http://localhost:2000/add";
+const classes=[1,2,3,4,5,6,7,8,9,10,11,12];
+console.log(classes)
 class Form extends Component {
   state={
       form:{
@@ -20,17 +24,22 @@ class Form extends Component {
         Country:"",
         Phone:null,
         Email:"",
-        Code:null
+        Code:null,
+        DOB:""
       },
       formErrorMessage: {
         ParentNameError: "",
         ChildNameError: "",
-        ageError: ""
+        EmailError:"",
+        ageError: "",
+        dobError:""
       },
       formValid: {
-        ParentName: false,
-        ChildName: false,
+        ParentNameValid: false,
+        ChildNameValid: false,
+        Email:false,
         age: false,
+        DOB:false,
         buttonActive: false
       },
       errorMessage: "",
@@ -52,44 +61,58 @@ class Form extends Component {
     switch (fieldName) {
         case "ParentName":
             if (value.length == 0) {
-                formValid.ParentName = false
-                formErrorMessage.ParentNameError = "field required"
+                formValid.ParentNameValid = false
+                formErrorMessage.ParentNameError = "Field Required"
             }
             else if (!value.match(/^[A-Za-z]{1,15}$/)) {
-                formValid.ParentName = false
-                formErrorMessage.ParentNameError = "Please enter a valid firstName"
+                formValid.ParentNameValid = false
+                formErrorMessage.ParentNameError = "Please enter a valid ParentName"
             }
             else {
-                formValid.ParentName = true
+                formValid.ParentNameValid = true
                 formErrorMessage.ParentNameError = ""
             }
             break;
         case "ChildName":
             if (value.length == 0) {
-                formValid.ChildName = false
-                formErrorMessage.ChildNameError = "field required"
+                formValid.ChildNameValid = false
+                formErrorMessage.ChildNameError = "Field Required"
             }
             else if (!value.match(/^[A-Za-z]{1,15}$/)) {
-                formValid.ChildName = false
-                formErrorMessage.ChildNameError = "Please enter a valid lastName"
+                formValid.ChildNameValid = false
+                formErrorMessage.ChildNameError = "Please enter a valid StudentName"
             }
             else {
-                formValid.ChildName = true
+                formValid.ChildNameValid = true
                 formErrorMessage.ChildNameError = ""
             }
             break;
-            case "age":
+            case "Email":
+            if (value.length == 0) {
+                formValid.Email = false
+                formErrorMessage.EmailError = "Field Required"
+            }
+            else if (!value.match(/^[a-zA-Z0-9 . _]+@[a-zA-Z0-9]+\.[a-zA-Z]{3,}$/)) {
+                formValid.Email = false
+                formErrorMessage.EmailError = "Please enter a valid Email"
+            }
+            else {
+                formValid.Email = true
+                formErrorMessage.EmailError = ""
+            }
+            break;
+            case "DOB":
               if (value.length == 0) {
-                  formValid.lastName = false
-                  formErrorMessage.lastNameError = "field required"
+                  formValid.DOB = false
+                  formErrorMessage.dobError = "field required"
               }
-              else if (Number(value)>=1 && Number(value)<70) {
-                  formValid.age = false
-                  formErrorMessage.ageError = "Please enter a valid age"
+              else if (new Date(value)>=new Date()) {
+                  formValid.DOB = false
+                  formErrorMessage.dobError = "Please Check"
               }
               else {
-                  formValid.age = true
-                  formErrorMessage.ageError = ""
+                  formValid.DOB = true
+                  formErrorMessage.dobError = ""
               }
               break;
 
@@ -126,10 +149,13 @@ class Form extends Component {
 //   }
 
   render(){
-  var {ParentName,ChildName,Grade,Country,Phone,Email,Code}=this.state.form;
+  var {ParentName,ChildName,Grade,Country,Phone,Email,Code,DOB}=this.state.form;
+  var {ParentNameError,ChildNameError,EmailError,dobError}=this.state.formErrorMessage;
+  
   return (
    /* <Model isOpen={this.props.isOpen} onRequestClose={()=>this.props.changeState()}  > */
    <div className="container-fluid">
+       {JSON.stringify(this.state)}
    <div className="row">
    <div className="col-lg-6 col-md-6 d-none d-md-block image-container">
     
@@ -145,41 +171,56 @@ class Form extends Component {
             <form>
                 <div className="form-input">
                 <span><RiParentLine/></span>
-                <input type="text" name="ParentName" value={ParentName} placeholder="Parent Name"/>
-                {/* <p className="text-danger text-left font-weight-bold ">This field is required</p> */}
+                <input type="text" name="ParentName" value={ParentName} placeholder="Parent Name" onChange={(e)=>this.handleChange(e)}/>
+                {ParentNameError && <p className="text-left text-danger font-weight-bold"><AiFillWarning size="1.5em"/>{ParentNameError}</p>}
                 </div>
-                
                 <div className="form-input">
                 <span><FaChild/></span>
-                <input type="text" name="ChildName" value={ChildName} placeholder="Child Name"/>
+                <input type="text" name="ChildName" value={ChildName} placeholder="Child Name" onChange={(e)=>this.handleChange(e)}/>
+                {ChildNameError && <p className="text-left text-danger font-weight-bold"><AiFillWarning size="1.5em"/>{ChildNameError}</p>}
                 </div>
                 <div className="form-input">
                 <span><FaBirthdayCake/></span>
-                <input type="text" name="ChildName" value={ChildName} placeholder="DOB"/>
+                <input type="text" name="DOB" value={DOB} placeholder="DOB"  onFocus={(e)=>e.target.type="date"} onChange={(e)=>this.handleChange(e)}/>
+                {dobError && <p className="text-left text-danger font-weight-bold"><AiFillWarning size="1.5em"/>{dobError}</p>}
                 </div>
                 <div className="form-input">
                 <span><MdSchool/></span>
-                <input type="text" name="Grade" value={Grade} placeholder="Grade"/>
+                <select type="text" name="Grade" value={Grade} placeholder="Grade" onChange={(e)=>this.handleChange(e)}>
+                    <option value="">Select Grade</option>
+                    {classes.map((data)=>{
+                        return <option value={data}>{data}</option>
+                    })
+
+                    }
+                </select>
                 </div>
                 <div className="form-input">
                 <span><TiContacts/></span>
-                <input type="number" name="Phone" value={Phone} placeholder="Contact"/>
+                <input type="number" name="Phone" value={Phone} min="0" placeholder="Contact" onChange={(e)=>this.handleChange(e)}/>
                 </div>
                 <div className="form-input">
                 <span><MdEmail/></span>
-                <input type="text" name="Email" value={Email} placeholder="Email"/>
+                <input type="text" name="Email" value={Email} placeholder="Email" onChange={(e)=>this.handleChange(e)}/>
+                {EmailError && <p className="text-left text-danger font-weight-bold"><AiFillWarning size="1.5em"/>{EmailError}</p>}
                 </div>
                 <div className="form-input">
                 <span><FaLandmark/></span>
-                <input type="text" name="Country" value={Country} placeholder="Country"/>
+                <select type="text" name="Country" value={Country} onChange={(e)=>this.handleChange(e)}>
+                    <option value="">Select Country</option>
+                    {country.map((data)=>{
+                        return <option value={data.label}>{data.label}</option>
+                    })
+
+                    }
+                </select>
                 </div>
                 <div className="form-input">
                 <span><RiLockPasswordLine/></span>
-                <input type="text" name="Code" value={Code} placeholder="Code"/>
+                <input type="text" name="Code" value={Code} placeholder="Code" onChange={(e)=>this.handleChange(e)}/>
                 </div>
                 <div className="text-center mb-3">
                     <button type="submit" className="btn">Submit</button>
-
                 </div>
             </form>
        </div>
